3ff8365a3674e3cf0170f18b81c6203513ca8861
EthanPortman <ethanportman@gmail.com>
2024-02-06 17:08:05 -0800

    Added a Header Component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
         <meta name="description" content="A simple message board app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      <Header />
     </>
   );
 }
