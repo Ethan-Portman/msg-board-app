8e3cf545d8fc4a93b5a0dfe54ec2843b6470e11b
EthanPortman <ethanportman@gmail.com>
2024-02-17 23:46:56 -0800

    Added summary comment

--- components/Authentication/AuthenticationError.js
+++ components/Authentication/AuthenticationError.js
@@ -1,16 +1,22 @@
+/*
+This component displays an alert for login or registration errors.
+   - Utilizes the React Bootstrap Alert component to show an informative message.
+
+When rendered, the alert is dismissible, allowing users to close it. 
+*/
+
 import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 
-function AuthenticationError({ error }) {
+
+const AuthenticationError = ({ error }) => {
     const [show, setShow] = useState(true);
 
     if (show) {
         return (
             <Alert variant="danger" onClose={() => setShow(false)} dismissible>
                 <Alert.Heading>Oh snap!</Alert.Heading>
-                <p>
-                    {error}
-                </p>
+                <p> {error} </p>
             </Alert>
         );
     }
