48e65f407ddef0ef5dadbe9bef5b28ba28e4de5f
EthanPortman <ethanportman@gmail.com>
2024-02-19 14:28:39 -0800

    Added description

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 // _app.js
-import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from "@/components/Authentication/AuthContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   );
-}
+};
