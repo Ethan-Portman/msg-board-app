01324c4f0e9b0799481b79ee0755272e963d7f7a
EthanPortman <ethanportman@gmail.com>
2024-02-19 14:29:02 -0800

    Fixed description

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
-// _app.js
+/*
+This file is a wrapper file for the entire application. 
+
+The Entire Application gets
+  - Bootstrap Styles
+  - AuthProvider Wrapper to provide the useAuth context to all components in the app
+*/
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from "@/components/Authentication/AuthContext";
 
