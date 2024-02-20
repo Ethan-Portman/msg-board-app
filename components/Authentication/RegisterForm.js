1fe442e31eee959f140b31ee70ab4c79df5f9ff3
EthanPortman <ethanportman@gmail.com>
2024-02-19 23:37:42 -0800

    Form now inherits from FormParent

--- components/Authentication/RegisterForm.js
+++ components/Authentication/RegisterForm.js
@@ -19,119 +19,43 @@ import { Formik } from 'formik';
 import * as yup from 'yup';
 import AuthenticationError from './AuthenticationError';
 import { registerSchema } from './FormSchemas';
+import FormParent from './FormParent';
 
-const RegisterForm = ({ handleRegister }) => {
-    const [registerError, setRegisterError] = useState(null);
-    const [showRegisterSuccess, setShowRegisterSuccess] = useState(false);
+const RegisterForm = ({ handleSubmit }) => {
+    const [submitError, setSubmitError] = useState(null);
 
     const handleFormData = async (values, { resetForm, setSubmitting }) => {
         try {
-            await handleRegister(values);
+            await handleSubmit(values);
             resetForm();
-            setRegisterError(null);
-            setShowRegisterSuccess(true);
+            setSubmitError(null);
             setSubmitting(false);
         } catch (error) {
             if (error.response.status === 409) {
-                setRegisterError('Username is already taken. Please choose a different username.');
+                setSubmitError('Username is already taken. Please choose a different username.');
             } else if (error.response.status === 400) {
-                setRegisterError('Error setting up the request:');
+                setSubmitError('Error setting up the request:');
             }
             setSubmitting(false);
         }
     };
 
-
     return (
         <Card className="mb-3">
             <Card.Body>
                 {/* Formik component for handling form state and validation */}
-                <Formik
+                <FormParent
                     validationSchema={registerSchema}
                     onSubmit={handleFormData}
-                    initialValues={{ name: '', password: '' }}
-                >
-                    {({
-                        handleSubmit,
-                        handleChange,
-                        handleBlur,
-                        values,
-                        touched,
-                        errors,
-                    }) => (
-                        <Form noValidate onSubmit={handleSubmit}>
-                            {/* Form field for entering a name */}
-                            <Form.Group as={Row} className="align-items-center mb-3" controlId="name">
-                                <Col xs={12}>
-                                    <Form.Label>Username:</Form.Label>
-                                </Col>
-                                <Col xs={12}>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Username"
-                                        value={values.name}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        isValid={touched.name && !errors.name}
-                                        isInvalid={touched.name && errors.name}
-                                    />
-                                    {/* Validation feedback for the name field */}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.name}
-                                    </Form.Control.Feedback>
-                                </Col>
-                            </Form.Group>
-
-                            {/* Form field for entering a password */}
-                            <Form.Group as={Row} className="align-items-center mb-3" controlId="password">
-                                <Col xs={12}>
-                                    <Form.Label>Password</Form.Label>
-                                </Col>
-                                <Col xs={12}>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Your password"
-                                        value={values.password}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        isValid={touched.password && !errors.password}
-                                        isInvalid={touched.password && errors.password}
-                                    />
-                                    {/* Validation feedback for the password field */}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.password}
-                                    </Form.Control.Feedback>
-                                </Col>
-                            </Form.Group>
-
-                            {/* Submit button */}
-                            <Row>
-                                <Col>
-                                    <Button variant="primary" type="submit" className="mt-3">
-                                        Register
-                                    </Button>
-                                </Col>
-
-                                <Col>
-                                    <Toast show={showRegisterSuccess} onClose={() => setShowRegisterSuccess(false)} className="mb-5">
-                                        <Toast.Header>
-                                            <strong className="me-auto">Registration Complete</strong>
-                                        </Toast.Header>
-                                        <Toast.Body>User <strong>{values.name}</strong> created </Toast.Body>
-                                    </Toast>
-                                </Col>
-
-                            </Row>
-
-                            {registerError && (  // Display the Error
-                                <AuthenticationError error={registerError} />
-                            )}
-                        </Form>
-                    )}
-                </Formik>
+                    submitError={submitError}
+                    btnTitle={"register"}
+                />
             </Card.Body>
-        </Card>
+        </Card >
     );
 };
 
 export default RegisterForm;
+
+
+
